8db088a47438d2d6ec228fdd1cc74dfe32c15942
Taylor Riley <t_riley13@icloud.com>
2023-09-13 17:14:41 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log(e.target.new-task-description.value)
+  })
 });
