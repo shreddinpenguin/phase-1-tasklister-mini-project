5215532721efd2c099803ce5cdc7369f33b04916
Taylor Riley <t_riley13@icloud.com>
2023-09-14 09:55:56 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("form").addEventListener("submit", (e) => {
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
     e.preventDefault()
-    console.log(e.target.new-task-description.value)
+    renderTask(e.target["new-task-description"].value)
+    form.reset()
   })
+  function renderTask (task) {
+    let li = document.createElement("li")
+    li.textContent = `${task} `
+    document.querySelector("#tasks").appendChild(li)
+    let btn = document.createElement("button")
+    btn.textContent = "x"
+    li.appendChild(btn)
+    btn.addEventListener("click", (e) => {
+      e.target.parentNode.remove()
+    })
+  }
 });
